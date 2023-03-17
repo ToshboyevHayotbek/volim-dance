ab37cad401372614360ea12906ab55f6164a5ba8
Your Name <you@example.com>
2023-03-17 23:12:55 +0100

    first commit

---
+++ js/node.js
@@ -0,0 +1,13 @@
+let isName = prompt('Ismingiz nima')
+
+if(true){
+    let nuName = prompt('Telefon nomeringiz')
+    alert('assalomu aleykum ' + " " + isName + ' bizning saytga xush kelibsiz')
+    console.log(nuName);
+}
+
+else(false)(
+    let faName = prompt('Otangizni ismi nima')
+    let moName = prompt('Onangizni ismi nima')
+    alert('assalomu aleykum ' + faName + " " + moName + ' bizning saytga xush kelibsiz')
+)
